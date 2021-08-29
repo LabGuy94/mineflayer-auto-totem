7b68a90dbf7c29c339adc6ff92a4d72bed34c16c
Link <50463727+link-discord@users.noreply.github.com>
2021-08-29 10:27:27 +0200

    Fixed an issue

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ module.exports = function inject(bot) {
     bot.autototem.equip = () => {
         if (bot.inventory.slots[45] != null) return
         const totem = bot.inventory.findInventoryItem('totem_of_undying', null, null)
-        if (totem) bot.equip(totem, 'off-hand')
+        if (totem) {
+            bot.inventory.requiresConfirmation = false
+            bot.equip(totem, 'off-hand')
+        }
     }
 }
